9e5aa988f7e3283973508b19a464985b0fe2a654
Joey Chen <jay.hatelove@gmail.com>
2023-08-29 21:29:05 +0800

    extract method: overlapping_days()

--- budget/budget.py
+++ budget/budget.py
@@ -15,8 +15,7 @@ class BudgetService:
             filter_budgets = list(filter(lambda b: b.year_month == start.strftime('%Y%m'), budgets))
             if len(filter_budgets) == 0:
                 return 0
-            overlapping_days = end.day - start.day + 1
-            return overlapping_days * filter_budgets[0].daily_amount()
+            return (end.day - start.day + 1) * filter_budgets[0].daily_amount()
 
         else:  # cross month
 
@@ -29,20 +28,24 @@ class BudgetService:
                     continue
 
                 budget = filter_budgets[0]
-                if budget.year_month == start.strftime('%Y%m'):
-                    overlapping_end = budget.last_day()
-                    overlapping_start = start
-                elif budget.year_month == end.strftime('%Y%m'):
-                    overlapping_end = end
-                    overlapping_start = budget.first_day()
-                else:
-                    overlapping_end = budget.last_day()
-                    overlapping_start = budget.first_day()
-                overlapping_days = (overlapping_end - overlapping_start).days + 1
+                overlapping_days = self.overlapping_days(budget, end, start)
                 total_amount += overlapping_days * budget.daily_amount()
                 current = current + relativedelta(months=1)
 
             return total_amount
+
+    def overlapping_days(self, budget, end, start):
+        if budget.year_month == start.strftime('%Y%m'):
+            overlapping_end = budget.last_day()
+            overlapping_start = start
+        elif budget.year_month == end.strftime('%Y%m'):
+            overlapping_end = end
+            overlapping_start = budget.first_day()
+        else:
+            overlapping_end = budget.last_day()
+            overlapping_start = budget.first_day()
+        overlapping_days = (overlapping_end - overlapping_start).days + 1
+        return overlapping_days
 
 
 class Budget:
