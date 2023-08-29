9c7e745144317dd3242d216b75acd4a5050ff559
Joey Chen <jay.hatelove@gmail.com>
2023-08-29 21:43:48 +0800

    inline temp variables: first_day and last_day

--- budget/budget.py
+++ budget/budget.py
@@ -11,10 +11,8 @@ class Period:
 
     def overlapping_days(self, budget):
         another = Period(budget.first_day(), budget.last_day())
-        last_day = another.end
-        first_day = another.start
-        overlapping_end = last_day if last_day < self.end else self.end
-        overlapping_start = first_day if first_day > self.start else self.start
+        overlapping_end = another.end if another.end < self.end else self.end
+        overlapping_start = another.start if another.start > self.start else self.start
         return (overlapping_end - overlapping_start).days + 1
 
 
