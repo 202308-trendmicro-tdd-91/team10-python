d4fb79a5948d095d98532c5db7c0fe42feae9135
Joey Chen <jay.hatelove@gmail.com>
2023-08-29 21:24:50 +0800

    use budget.first_day() for overlapping_days when end month

--- budget/budget.py
+++ budget/budget.py
@@ -33,9 +33,9 @@ class BudgetService:
                 budget = filter_budgets[0]
                 if current_year_month == start_year_month:
                     overlapping_days = (budget.last_day() - start).days + 1
-                    # overlapping_days = calendar.monthrange(start.year, start.month)[1] - start.day + 1
                 elif current_year_month == end_year_month:
-                    overlapping_days = end.day
+                    overlapping_days = (end - budget.first_day()).days + 1
+                    # overlapping_days = end.day
                 else:
                     overlapping_days = calendar.monthrange(current.year, current.month)[1]
                 total_amount += overlapping_days * budget.daily_amount()
@@ -48,6 +48,9 @@ class Budget:
     def __init__(self, year_month, amount):
         self.year_month = year_month
         self.amount = amount
+
+    def first_day(self):
+        return datetime.strptime(self.year_month, '%Y%m').date()
 
     def last_day(self):
         return datetime.strptime(self.year_month + str(self.days()), '%Y%m%d').date()
