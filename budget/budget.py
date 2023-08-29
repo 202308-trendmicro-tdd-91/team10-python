37625144a1aacd94eb50d5cdded0710cffa97396
Joey Chen <jay.hatelove@gmail.com>
2023-08-29 21:40:42 +0800

    remove statement

--- budget/budget.py
+++ budget/budget.py
@@ -11,11 +11,11 @@ class Period:
 
     def overlapping_days(self, budget):
         if budget.year_month == self.start.strftime('%Y%m'):
-            overlapping_start = self.start
+            pass
         elif budget.year_month == self.end.strftime('%Y%m'):
-            overlapping_start = budget.first_day()
+            pass
         else:
-            overlapping_start = budget.first_day()
+            pass
         overlapping_end = budget.last_day() if budget.last_day() < self.end else self.end
         overlapping_start = budget.first_day() if budget.first_day() > self.start else self.start
         return (overlapping_end - overlapping_start).days + 1
